881e9d8669393fec0121231070635834d1b04c25
Junhao Li <jl2922@cornell.edu>
2017-12-23 16:53:03 -0500

    setup and single int serialization (#1)

---
+++ src/int_serializer.h
@@ -0,0 +1,32 @@
+#ifndef HPS_INT_SERIALIZER_H_
+#define HPS_INT_SERIALIZER_H_
+
+#include <cassert>
+#include <iostream>
+#include <type_traits>
+#include "uint_serializer.h"
+
+namespace hps {
+
+// Serialize and parse unsigned integer types with base 127 zigzag encoding.
+
+template <class T>
+typename std::enable_if<std::is_signed<T>::value, void>::type serialize(
+    const T& num, std::ostream& stream) {
+  const int n_bits = sizeof(num) * 8;
+  unsigned long long zigzaged_num = (num << 1) ^ (num >> (n_bits - 1));
+  serialize(zigzaged_num, stream);
+}
+
+template <class T>
+typename std::enable_if<std::is_signed<T>::value, void>::type parse(
+    T* num_ptr, std::istream& stream) {
+  unsigned long long zigzaged_num;
+  parse(&zigzaged_num, stream);
+  *num_ptr = -(zigzaged_num & 1);
+  *num_ptr ^= zigzaged_num >> 1;
+}
+
+}  // namespace hps
+
+#endif
