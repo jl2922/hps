594805872b8a093bfb3cddbdf5f68cd74a08cea3
Junhao Li <junhao@Junhaos-MacBook-Pro.local>
2017-12-24 18:52:38 -0500

    .

---
+++ src/basic_types/basic_types.h
@@ -0,0 +1,9 @@
+#ifndef HPS_BASIC_TYPES_H_
+#define HPS_BASIC_TYPES_H_
+
+#include "float_serializer.h"
+#include "int_serializer.h"
+#include "string_serializer.h"
+#include "uint_serializer.h"
+
+#endif
