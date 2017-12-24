58403b7ead5d645141b799f39a4e344f61e6882b
Junhao Li <junhao@Junhaos-MacBook-Pro.local>
2017-12-24 11:18:09 -0500

    .

--- src/serializer.h
+++ src/serializer.h
@@ -2,6 +2,7 @@
 #define HPS_SERIALIZER_H_
 
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <typeinfo>
 
@@ -14,14 +15,14 @@ class Serializer {
     (void)t;
     (void)stream;
     const std::string type_name = typeid(T).name();
-    throw std::runtime_error("serialize not implemented for type " + type_name);
+    throw std::logic_error("serialize not implemented for type " + type_name);
   }
 
   static void parse(T& t, std::istream& stream) {
     (void)t;
     (void)stream;
     const std::string type_name = typeid(T).name();
-    throw std::runtime_error("parse not implemented for type " + type_name);
+    throw std::logic_error("parse not implemented for type " + type_name);
   }
 };
 
