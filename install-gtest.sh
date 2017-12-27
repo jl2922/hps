20c17268b0ca24c9fb434c1a71a55a7473682b8d
Junhao Li <junhao@Junhaos-MacBook-Pro.local>
2017-12-27 15:22:28 -0500

    .

--- install-gtest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Downloading Google Test"
-wget -O release-1.8.0.tar.gz https://github.com/google/googletest/archive/release-1.8.0.tar.gz
-tar xzf release-1.8.0.tar.gz
-rm release-1.8.0.tar.gz
-mv googletest-release-1.8.0 gtest
-echo "Completed"
-echo
