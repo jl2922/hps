8f36c0f5f066bf34bb23613e15bbfc2fbc8252d2
Junhao Li <jl2922@cornell.edu>
2018-02-09 11:55:06 -0500

    optimize string parse (#26)

--- src/basic_type/string_serializer.h
+++ src/basic_type/string_serializer.h
@@ -14,30 +14,15 @@ class Serializer<std::string, B> {
   static void serialize(const std::string& str, OutputBuffer<B>& ob) {
     const size_t n_bytes = str.size();
     Serializer<size_t, B>::serialize(n_bytes, ob);
-    ob.write(str.c_str(), n_bytes);
+    ob.write(str.data(), n_bytes);
   }
 
   static void parse(std::string& str, InputBuffer<B>& ib) {
     size_t n_bytes;
     Serializer<size_t, B>::parse(n_bytes, ib);
-    char buf[PARSE_BUFFER_SIZE];
-    str.clear();
-    str.reserve(n_bytes);
-    while (n_bytes > 0) {
-      if (n_bytes > PARSE_BUFFER_SIZE) {
-        ib.read(buf, PARSE_BUFFER_SIZE);
-        str.append(buf, PARSE_BUFFER_SIZE);
-        n_bytes -= PARSE_BUFFER_SIZE;
-      } else {
-        ib.read(buf, n_bytes);
-        str.append(buf, n_bytes);
-        n_bytes = 0;
-      }
-    }
+    str.resize(n_bytes);
+    ib.read(&str[0], n_bytes);
   }
-
- private:
-  constexpr static size_t PARSE_BUFFER_SIZE = 1 << 10;
 };
 
 }  // namespace hps
