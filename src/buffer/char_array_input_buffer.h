fc300becb32a08ce677a14dca7b7e730e8b48e3e
Junhao Li <streaver91@gmail.com>
2018-02-20 17:53:25 -0500

    .

---
+++ src/buffer/char_array_input_buffer.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "input_buffer.h"
+
+#include <cstring>
+
+namespace hps {
+
+template <>
+class InputBuffer<char*> {
+ public:
+  InputBuffer(const char* arr) : arr(arr) { pos = 0; }
+
+  void read(char* content, size_t length) {
+    strncpy(content, &arr[pos], length);
+    pos += length;
+  }
+
+  char read_char() {
+    const char ch = arr[pos];
+    pos++;
+    return ch;
+  }
+
+ private:
+  const char* const arr;
+
+  size_t pos;
+};
+
+}  // namespace hps
