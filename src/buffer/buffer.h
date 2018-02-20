8cb26c4f364291dca61c75c413adb59f918471bc
Junhao Li <streaver91@gmail.com>
2018-02-20 18:43:27 -0500

    .

--- src/buffer/buffer.h
+++ src/buffer/buffer.h
@@ -1,6 +1,7 @@
 #ifndef HPS_BUFFER_H_
 #define HPS_BUFFER_H_
 
+#include "char_array_input_buffer.h"
 #include "stream_input_buffer.h"
 #include "stream_output_buffer.h"
 #include "string_input_buffer.h"
