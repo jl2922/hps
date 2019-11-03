0929f91487edcf895dfde2a078d0a72b3f669d53
BRULE Herman <alpha_one_x86@first-world.info>
2019-11-02 21:28:51 -0400

    Add some comment

--- src/buffer/stream_input_buffer.h
+++ src/buffer/stream_input_buffer.h
@@ -47,6 +47,7 @@ class StreamInputBuffer {
     return *this;
   }
   
+  //usefull to debug and profile the file size
   inline size_t tellg() const {
       return (size_t)stream->tellg() - (size_t)STREAM_INPUT_BUFFER_SIZE + (size_t)pos;
   }
--- src/buffer/stream_output_buffer.h
+++ src/buffer/stream_output_buffer.h
@@ -47,6 +47,7 @@ class StreamOutputBuffer {
     return *this;
   }
   
+  //usefull to debug and profile the file size
   inline size_t tellp() const {
       return (size_t)stream->tellp() + pos;
   }
